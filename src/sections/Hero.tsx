ece7537f420d29db8925671bc844e20be2a0e3af
junaidapdev <junaidap.dev@gmail.com>
2024-08-20 13:05:06 +0530

    changes

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -62,12 +62,12 @@ export const HeroSection = () => {
           <Image src={memojiImage} className="size-[100px]" alt="Person" />
             <div className="bg-gray-950 border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg ">
               <div className="bg-green-500 size-2.5 rounded-full"></div>
-                <div className="text-sm font-medium">Availabe for New Project</div>
+                <div className="text-sm font-medium">മലയാളത്തിൽ പഠിക്കാം!</div>
             </div>
         </div>
         <div className="max-w-lg mx-auto">
-          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Learn to Code in Malayalam</h1>
-          <p className="mt-4 text-center text-white/60 md:text-lg">Hello Malayalees!dvwedv!!!</p>
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Learn to Code, Build Websites, and Get Hired—All in Malayalam.</h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">Join live online classes with industry experts and transform your career in tech.</p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
