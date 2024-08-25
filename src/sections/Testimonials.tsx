5b63753687948734b73fbf9425a63ab6e0bb41c1
junaidapdev <junaidap.dev@gmail.com>
2024-08-25 17:10:12 +0530

    testimonial-animation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -7,6 +7,7 @@ import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
 import grainImage from "@/assets/images/grain.jpg" 
 import { Card } from "@/components/Card";
+import { Fragment } from "react";
 
 const testimonials = [
   {
@@ -47,10 +48,14 @@ export const TestimonialsSection = () => {
   <div className="py-16 lg:py-24">
     <div className="container">
     <SectionHeader eyebrow="Happy Students" title="What Students are saying about us" description="Poli Poli Poli"/>
-      <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent, black_10%, black90%,transparent)]">
-        <div className="flex gap-8 flex-none">
-        {testimonials.map((testimonial) =>(
-          <Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8">
+      <div className="mt-12 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4">
+        <div className="flex gap-8 pr-8 flex-none animate-move-left [animation-duration:90s] hover:[animation-play-state:paused]">
+
+        {[...new Array(2)].fill(0).map((_, index) => (
+        <Fragment key={index}>
+          {testimonials.map((testimonial) =>(
+          <Card key={testimonial.name} className="max-w-xs md:max-w-md p-6 md:p-8 hover:-rotate-3 transition duration-300
+          ">
             <div className="flex gap-4 items-center">
               <div className="size-14 bg-gray-700 inline-flex items-center justify-center  rounded-full flex-shrink-0">
                 <Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full"/>
@@ -64,6 +69,10 @@ export const TestimonialsSection = () => {
             <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
             </Card>
         ))}
+        </Fragment>
+      ))}
+
+       
         </div>
       </div>  
     </div>
