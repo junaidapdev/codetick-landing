f7ad7cb48109bcf53db61449829f363c37bc31d1
junaidapdev <junaidap.dev@gmail.com>
2024-08-25 17:14:14 +0530

    header-update

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,10 +1,10 @@
 export const Header = () => {
   return <div className="flex justify-center items-center fixed top-3 w-full z-10">
     <nav className="flex gap-1 p-0.5 border-white/15 rounded-full bg-white/10 backdrop-blur">
-      <a href="#" className="nav-item">Home</a>
-      <a href="#" className="nav-item">Projects</a>
-      <a href="#" className="nav-item">About</a>
-      <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
+      <a href="#" className="nav-item">codetick.live - learn to code in malayalam</a>
+      {/* <a href="#" className="nav-item">learn to </a>
+      <a href="#" className="nav-item">code</a> */}
+      {/* <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a> */}
     </nav>
   </div>;
 };
